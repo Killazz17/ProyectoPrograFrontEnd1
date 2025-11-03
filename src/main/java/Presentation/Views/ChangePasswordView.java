88a42b8c96b96be90680c73f3db2eb8ea73d48fe
FSPABLO <josefdezs2110@gmail.com>
2025-11-03 09:55:15 -0600

    Se agrego ChangePasswordView

---
+++ src/main/java/Presentation/Views/ChangePasswordView.java
@@ -0,0 +1,22 @@
+package Presentation.Views;
+
+import javax.swing.*;
+
+public class ChangePasswordView {
+    private JPanel ContentPane;
+    private JPanel FormPanel;
+    private JPanel TittleLabelPanel;
+    private JLabel ChangePasswordLabel;
+    private JTextField UserField;
+    private JLabel UserLabel;
+    private JPanel UserFieldPanel;
+    private JPasswordField ActualPasswordField;
+    private JLabel ActualPasswordLabel;
+    private JPanel ActualPasswordFieldPanel;
+    private JPasswordField NewPasswordField;
+    private JLabel NewPasswordLabel;
+    private JPanel NewPasswordPanel;
+    private JPanel ButtonPanel;
+    private JButton CancelButton;
+    private JButton AcceptButton;
+}
